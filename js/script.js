cb6ac97d47db1424e237c677dbce54179e849f2a
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-22 17:18:13 +0000

    Changed rectangle to triangle and added a custom triangle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-// Cristiano | ICS2O-Assignment2-HTML | March 11 2022
+// Cristiano
+// ICS2O-Assignment2-HTML
+// March 11 2022
 
 
 /**
- * Calculates area for a rectangle.
+ * Calculates area for a triangle.
  */
 function calculateArea(elementid, length, width) {
-  document.getElementById(elementid).innerHTML = "<p>The area of this rectangle is " + (length * width) + "cm²</p>" 
+  if (elementid != "triangle3answer") {
+    document.getElementById(elementid).innerHTML = "<p>The area of this triangle is " + (length * width / 2) + "cm²</p>" 
+  } else {
+    const lengthcustom = parseInt(document.getElementById("lengthcustom").value)
+    const widthcustom = parseInt(document.getElementById("widthcustom").value)
+    document.getElementById(elementid).innerHTML = "<p>The area of this triangle is " + (lengthcustom * widthcustom / 2) + "cm²</p>" 
+  }
 }
